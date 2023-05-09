b2afb966a511294d4dff408a5855f9331bee3ea5
Olumide04 <ayeromara12@gmail.com>
2023-05-09 21:06:39 +0000

    commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,33 +1,16 @@
-Write a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None.
+import requests
 
-Hint: The Reddit API uses pagination for separating pages of responses.
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"limit": 100, "after": after}
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        return None
+    data = response.json()
+    for post in data["data"]["children"]:
+        hot_list.append(post["data"]["title"])
+    if data["data"]["after"] is not None:
+        recurse(subreddit, hot_list, data["data"]["after"])
+    return hot_list
 
-Requirements:
-
-Prototype: def recurse(subreddit, hot_list=[])
-Note: You may change the prototype, but it must be able to be called with just a subreddit supplied. AKA you can add a counter, but it must work without supplying a starting value in the main.
-If not a valid subreddit, return None.
-NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
-Your code will NOT pass if you are using a loop and not recursively calling the function! This /can/ be done with a loop but the point is to use a recursive function. :)
-
-wintermancer@lapbox ~/reddit_api/project $ cat 2-main.py
-#!/usr/bin/python3
-"""
-2-main
-"""
-import sys
-
-if __name__ == '__main__':
-    recurse = __import__('2-recurse').recurse
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
-wintermancer@lapbox ~/reddit_api/project $ python3 2-main.py programming
-932
-wintermancer@lapbox ~/reddit_api/project $ python3 2-main.py this_is_a_fake_subreddit
-None
