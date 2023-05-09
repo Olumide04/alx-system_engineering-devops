29e764e3500820573ad8fe2f87e80b257f7c3238
Olumide04 <ayeromara12@gmail.com>
2023-05-09 21:09:28 +0000

    commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,16 +1,25 @@
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {"limit": 100, "after": after}
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, params=params, headers=headers)
-    if response.status_code != 200:
+    """
+    Returns a list containing the titles of all hot articles for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    params = {'limit': 100, 'after': after}
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    else:
         return None
-    data = response.json()
-    for post in data["data"]["children"]:
-        hot_list.append(post["data"]["title"])
-    if data["data"]["after"] is not None:
-        recurse(subreddit, hot_list, data["data"]["after"])
-    return hot_list
 
